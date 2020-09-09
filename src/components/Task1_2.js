87f24d664c3119590d0c74258e00d6eab6184793
DomeNTPS <tanndome_555@hotmail.com>
2020-09-09 14:57:16 +0700

    add random weight

--- src/components/Task1_2.js
+++ src/components/Task1_2.js
@@ -8,9 +8,9 @@ export const Task1_2 = () => {
     // const pos = {x:'',y:''}
     // const [posi, setPosi] = React.useState(pos)
     const [posxString, setPosxString] = React.useState([])
-    const [posyString, setPosyString] = React.useState([]);
+    const [posyString, setPosyString] = React.useState([])
+    const [weight, setWeight] = React.useState([])
     
-
     const RiceImage = () => {
         const [image] = useImage(
             "http://128.199.244.46:4000/getimage?image=inferNone94323.jpeg"
@@ -36,23 +36,21 @@ export const Task1_2 = () => {
     //     }
     //     setRanPos(...ranpos, pos) 
     // };
-
     //// ดูตรง sortx นะ เพราะจะลองใส่แบบค่าเดียวก่อน
-
     const sortXY = () => {
-        let posx = [];
+        let posx = []; 
         const minx = 0;
         const maxx = 360;
         for (let index = 0; index < 50; index++) {
             let randx = Math.floor(minx + Math.random() * (maxx - minx));
             console.log(index, randx);
             posx.push(randx)
-            
         }
         setPosxString(posx);
         console.log('x',posx.join())
         // console.log(typeof posx)
         console.log(posxString)
+        //-------------------------
         let posy = [];
         const miny = 0;
         const maxy = 438;
@@ -76,9 +74,23 @@ export const Task1_2 = () => {
     //   return data.map((item, i) => <Circle key={i} x={item} y={300} stroke="yellow"></Circle>);
     // };
 
+    const randomWeight = () => {
+      let wg = []
+      const minWg = 0.00
+      const maxWg = 10.00
+      for (let index = 0; index < 50; index++) {
+        let ranWg =(minWg + Math.random() * (maxWg - minWg)).toFixed(2);
+        console.log(index, ranWg);
+        wg.push(ranWg)
+      }
+      setWeight(wg)
+      console.log(wg, 'kg')
+    }
+
     return (
       <div>
-        <button onClick={sortXY}>random x </button>
+        <button onClick={sortXY}>random xy </button>
+        <button onClick={randomWeight}>Random Weight</button>
         <div style={{ display: "flex" }}>
           <ul> x {renderPosx(posxString)}</ul>
           <ul> y {renderPosy(posyString)}</ul>
