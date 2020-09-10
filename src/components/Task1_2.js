bf5e2e754cced8d230a5b74468bfdf32d861293e
DomeNTPS <tanndome_555@hotmail.com>
2020-09-10 11:29:38 +0700

    merge all random to one button

--- src/components/Task1_2.js
+++ src/components/Task1_2.js
@@ -5,8 +5,8 @@ import useImage from "use-image";
 export const Task1_2 = () => {
 
     // const [ranpos, setRanPos] = React.useState([])
-    // const pos = {x:'',y:''}
-    // const [posi, setPosi] = React.useState(pos)
+    let pos = {x:0,y:0,weightps:0}
+    const [posi, setPosi] = React.useState(pos)
     const [posxString, setPosxString] = React.useState([])
     const [posyString, setPosyString] = React.useState([])
     const [weight, setWeight] = React.useState([])
@@ -24,20 +24,7 @@ export const Task1_2 = () => {
         );
     };
 
-    // const RandomPos = () => {
-    //     const minx = 0
-    //     const miny = 0
-    //     const maxx = 360
-    //     const maxy = 438
-    //     for (let index = 0; index < 50; index++) {
-    //        var randx = Math.floor(minx + Math.random() * (maxx - minx));
-    //        var randy = Math.floor(miny + Math.random() * (maxy - miny));
-    //        console.log(index,randx,randy);
-    //     }
-    //     setRanPos(...ranpos, pos) 
-    // };
-    //// ดูตรง sortx นะ เพราะจะลองใส่แบบค่าเดียวก่อน
-    const sortXY = () => {
+    const randomXYW = () => {
         let posx = []; 
         const minx = 0;
         const maxx = 360;
@@ -47,53 +34,56 @@ export const Task1_2 = () => {
             posx.push(randx)
         }
         setPosxString(posx);
+        setPosi(posi.x = posxString)
         console.log('x',posx.join())
-        // console.log(typeof posx)
-        console.log(posxString)
+        console.log(posi.x,'x')
+        // console.log(posxString)
         //-------------------------
         let posy = [];
         const miny = 0;
         const maxy = 438;
         for (let index = 0; index < 50; index++) {
           let randy = Math.floor(miny + Math.random() * (maxy - miny));
-          console.log(index, randy);
+          // console.log(index, randy);
           posy.push(randy);
         }
         setPosyString(posy);
-        console.log("y", posy.join());
-        console.log(posyString);
+        setPosi(posi.y = posyString);
+        console.log(posi.y, "y");
+        // console.log("y", posy.join());
+        // console.log(posyString);
+        //-------------------------
+        let wg = [];
+        const minWg = 0.0;
+        const maxWg = 10.0;
+        for (let index = 0; index < 50; index++) {
+          let ranWg = (minWg + Math.random() * (maxWg - minWg)).toFixed(2);
+          console.log(index, ranWg);
+          wg.push(ranWg);
+        }
+        setWeight(wg);
+        setPosi((posi.weightps = weight));
+        // console.log(weight)
+        // console.log(wg, 'kg')
+        console.log(posi.weightps);
+        //-------------------------
+        console.log(posi)
     }
 
-    const renderPosx = (data) => {
-      return data.map((item, i) => <li key={i}>{item}</li>);
-    };
-    const renderPosy = (data) => {
+    const renderPos = (data) => {
       return data.map((item, i) => <li key={i}>{item}</li>);
     };
     // const renderPosxCirclle = (data) => {
     //   return data.map((item, i) => <Circle key={i} x={item} y={300} stroke="yellow"></Circle>);
     // };
 
-    const randomWeight = () => {
-      let wg = []
-      const minWg = 0.00
-      const maxWg = 10.00
-      for (let index = 0; index < 50; index++) {
-        let ranWg =(minWg + Math.random() * (maxWg - minWg)).toFixed(2);
-        console.log(index, ranWg);
-        wg.push(ranWg)
-      }
-      setWeight(wg)
-      console.log(wg, 'kg')
-    }
-
     return (
       <div>
-        <button onClick={sortXY}>random xy </button>
-        <button onClick={randomWeight}>Random Weight</button>
+        <button onClick={randomXYW}>random xy </button>
         <div style={{ display: "flex" }}>
-          <ul> x {renderPosx(posxString)}</ul>
-          <ul> y {renderPosy(posyString)}</ul>
+          <ul> x {renderPos(posxString)}</ul>
+          <ul> y {renderPos(posyString)}</ul>
+          <ul> w {renderPos(weight)}</ul>
         </div>
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
