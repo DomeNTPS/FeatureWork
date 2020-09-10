be556690864828b0171562ba808efa470be7ec94
DomeNTPS <tanndome_555@hotmail.com>
2020-09-10 16:00:30 +0700

    can random draw circle

--- src/components/Task1_2.js
+++ src/components/Task1_2.js
@@ -4,7 +4,6 @@ import useImage from "use-image";
 
 export const Task1_2 = () => {
 
-    // const [ranpos, setRanPos] = React.useState([])
     let pos = {x:0,y:0,weightps:0}
     const [posi, setPosi] = React.useState([pos])
     const [posxString, setPosxString] = React.useState([])
@@ -30,39 +29,33 @@ export const Task1_2 = () => {
         const maxx = 360;
         for (let index = 0; index < 50; index++) {
             let randx = Math.floor(minx + Math.random() * (maxx - minx));
-            console.log(index, randx);
+            // console.log(index, randx);
             posx.push(randx)
         }
         setPosxString(posx);
-        console.log('x',posx.join())
-        console.log(posi.x,'x')
-        // console.log(posxString)
+        // console.log('x',posx.join())
+        // console.log(posi.x,'x')
         //-------------------------
         let posy = [];
         const miny = 0;
         const maxy = 438;
         for (let index = 0; index < 50; index++) {
           let randy = Math.floor(miny + Math.random() * (maxy - miny));
-          // console.log(index, randy);
           posy.push(randy);
         }
         setPosyString(posy);
-        console.log(posi.y, "y");
-        // console.log("y", posy.join());
-        // console.log(posyString);
+        // console.log(posi.y, "y");
         //-------------------------
         let wg = [];
         const minWg = 0.0;
         const maxWg = 10.0;
         for (let index = 0; index < 50; index++) {
           let ranWg = (minWg + Math.random() * (maxWg - minWg)).toFixed(2);
-          console.log(index, ranWg);
+          // console.log(index, ranWg,'iw');
           wg.push(ranWg);
         }
         setWeight(wg);
-        // console.log(weight)
-        // console.log(wg, 'kg')
-        console.log(posi.weightps);
+        // console.log(posi.weightps);
         //-------------------------
         let tempPosition = [];
         posx.map((posXData, idx) => {
@@ -72,42 +65,37 @@ export const Task1_2 = () => {
             weight: wg[idx],
           });
         });
-        console.log(posi)
+        setPosi(tempPosition);
+        // console.log(posi,'pos')
     }
 
-    const renderPos = (data) => {
-      return data.map((item, i) => <li key={i}>{item}</li>);
-    };
-    const renderCircle = (data) => {
-      return data.map((item, i) => (
-        <Circle x={item} y={item} radius={25} stroke="yellow" key={i}/>
-      ));
-    };
-    
+    // const renderPos = (data) => {
+    //   return data.map((item, i) => <li key={i}>{item}</li>);
+    // };
 
     return (
       <div>
         <button onClick={randomXYW}>random xy </button>
-        <div style={{ display: "flex" }}>
+        {/* <div style={{ display: "flex" }}>
           <ul> x {renderPos(posxString)}</ul>
           <ul> y {renderPos(posyString)}</ul>
           <ul> w {renderPos(weight)}</ul>
-        </div>
+        </div> */}
         <Stage width={window.innerWidth} height={window.innerHeight}>
           <Layer>
             <RiceImage />
-            {renderCircle(posxString)}
-            {/* problem -------------------------------
-            {posi.map((item, i) => (
-              <Circle
-                x={item.x}
-                y={300}
-                radius={25}
-                stroke="yellow"
-                key={i}
-              />
-            ))}
-            -------------------------------------------------- */}
+            {posi.map((item, i) => {
+              console.log(item);
+              return (
+                <Circle
+                  x={item.x}
+                  y={item.y}
+                  radius={25}
+                  stroke="yellow"
+                  key={i}
+                />
+              );
+            })}
           </Layer>
         </Stage>
       </div>
