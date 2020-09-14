9fc8db562e862c1a0c590b2853bb385c74ef4686
DomeNTPS <tanndome_555@hotmail.com>
2020-09-14 17:08:05 +0700

    add pass prop function

--- src/components/Task1.js
+++ src/components/Task1.js
@@ -3,43 +3,46 @@ import Typography from "@material-ui/core/Typography";
 import Slider from "@material-ui/core/Slider";
 import { useHistory } from "react-router";
 
-export const Task1 = () => {
+export const Task1 = ({ submit, handleSubmit }) => {
+  const { push } = useHistory();
+  const [value1, setValue] = useState([0, 10]);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    // console.log(newValue);
+  };
+  const changePage = () => {
+    push("/Task2");
+  };
+  const onSetProp = () => {
+    const data = value1
+    submit(data) 
+  };
 
-    const { push } = useHistory();
-    const [value1, setValue] = useState([0, 10]);
-    const handleChange = (event, newValue) => {
-      setValue(newValue);
-      console.log(newValue)
-    };
-    const changePage = () => {
-      push("/Task2")
-    } 
-
-    return (
-      <div style={{ marginLeft: 300 }}>
-        {/* <button onClick={changePage}> Task 2 </button> */}
-        <Typography
-          id="range-slider"
-          gutterBottom
-          style={{  marginTop: 100 }}
-        >
-          Slider range
-        </Typography>
-        <div style={{ width: 300 }}>
-          <Slider
-            value={value1}
-            onChange={handleChange}
-            valueLabelDisplay="auto"
-            track="normal"
-            defaultValue={10}
-            mix={0.0}
-            max={10.0}
-            step={0.01}
-          />
-          {value1[0]} - {value1[1]} 
-        </div>
+  return (
+    <div style={{ marginLeft: 300 }}>
+      {/* <button onClick={changePage}> Task 2 </button> */}
+      <Typography id="range-slider" gutterBottom style={{ marginTop: 100 }}>
+        Slider range
+      </Typography>
+      <div style={{ width: 300 }}>
+        <Slider
+          value={value1}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          track="normal"
+          defaultValue={10}
+          mix={0.0}
+          max={10.0}
+          step={0.01}
+        />
+        {value1[0]} - {value1[1]}
       </div>
-    );
-}
+      <br />
+      {/* <div>{textHint}</div> */}
+      <button onClick={handleSubmit}>submit</button>
+      <button onClick={onSetProp}>submit 2</button>
+    </div>
+  );
+};
 
 export default Task1;
--- src/components/Task1_4.js
+++ src/components/Task1_4.js
@@ -3,12 +3,24 @@ import Task1 from "./Task1";
 import Task2 from "./Task1_2";
 
 function Task1_4() {
+
+    const propHint = []
+    const onHandleProp = () => {
+        propHint.push();
+        console.log(propHint,'hi')
+    }
+    const handleSubmit = () => {
+      alert("set")
+    };
+
     return (
-        <div>
-            <Task1/>
-            <Task2/>
-        </div>
-    )
+      <div>
+        <Task1 submit={onHandleProp} />
+        <Task1 handleSubmit={handleSubmit} submit={onHandleProp} />
+        {/* <Task1 handleSubmit2={handleProp} /> */}
+        <Task2 />
+      </div>
+    );
 }
 
 export default Task1_4
