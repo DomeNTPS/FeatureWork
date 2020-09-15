7998567e43b5dfc5f6bbd85dcef5b909c0f52b82
DomeNTPS <tanndome_555@hotmail.com>
2020-09-15 14:31:13 +0700

    test fillter function

--- src/components/Task1_2.js
+++ src/components/Task1_2.js
@@ -2,98 +2,88 @@ import React from 'react'
 import { Stage, Layer, Circle, Image } from "react-konva";
 import useImage from "use-image";
 
-export const Task1_2 = () => {
+export const Task1_2 = ({ filterValue }) => {
+  let pos = { x: 0, y: 0, weightps: 0 };
+  const [posi, setPosi] = React.useState([pos]);
 
-    let pos = {x:0,y:0,weightps:0}
-    const [posi, setPosi] = React.useState([pos])
-    const [posxString, setPosxString] = React.useState([])
-    const [posyString, setPosyString] = React.useState([])
-    const [weight, setWeight] = React.useState([])
-    
-    const RiceImage = () => {
-        const [image] = useImage(
-            "http://128.199.244.46:4000/getimage?image=inferNone94323.jpeg"
-        );
-        return (
-          <Image
-            width={360}
-            height={438}
-            image={image}
-            x={300}
-          />
-        );
-    };
+  const RiceImage = () => {
+    const [image] = useImage(
+      "http://128.199.244.46:4000/getimage?image=inferNone94323.jpeg"
+    );
+    return <Image width={360} height={438} image={image} x={300} />;
+  };
 
-    const randomXYW = () => {
-        let posx = []; 
-        const minx = 0;
-        const maxx = 360;
-        for (let index = 0; index < 50; index++) {
-            let randx = Math.floor(minx + Math.random() * (maxx - minx));
-            // console.log(index, randx);
-            posx.push(randx)
-        }
-        setPosxString(posx);
-        // console.log('x',posx.join())
-        // console.log(posi.x,'x')
-        //-------------------------
-        let posy = [];
-        const miny = 0;
-        const maxy = 438;
-        for (let index = 0; index < 50; index++) {
-          let randy = Math.floor(miny + Math.random() * (maxy - miny));
-          posy.push(randy);
-        }
-        setPosyString(posy);
-        // console.log(posi.y, "y");
-        //-------------------------
-        let wg = [];
-        const minWg = 0.0;
-        const maxWg = 10.0;
-        for (let index = 0; index < 50; index++) {
-          let ranWg = (minWg + Math.random() * (maxWg - minWg)).toFixed(2);
-          // console.log(index, ranWg,'iw');
-          wg.push(ranWg);
-        }
-        setWeight(wg);
-        // console.log(posi.weightps);
-        //-------------------------
-        let tempPosition = [];
-        posx.map((posXData, idx) => {
-          tempPosition.push({
-            x: posXData,
-            y: posy[idx],
-            weight: wg[idx],
-          });
-        });
-        setPosi(tempPosition);
-        // console.log(posi,'pos')
+  const randomXYW = () => {
+    let posx = [];
+    const minx = 0;
+    const maxx = 360;
+    for (let index = 0; index < 50; index++) {
+      let randx = Math.floor(minx + Math.random() * (maxx - minx));
+      posx.push(randx);
     }
 
-    return (
-      <div>
-        <button onClick={randomXYW} style={{marginLeft:300, marginBottom: 50, marginTop: 100}}>
-          random xy{" "}
-        </button>
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-          <Layer>
-            <RiceImage />
-            {posi.map((item, i) => {
-              // console.log(item);
-              return (
-                <Circle
-                  x={item.x + 300}
-                  y={item.y}
-                  radius={25}
-                  stroke="yellow"
-                  key={i}
-                />
-              );
-            })}
-          </Layer>
-        </Stage>
+    //-------------------------
+    let posy = [];
+    const miny = 0;
+    const maxy = 438;
+    for (let index = 0; index < 50; index++) {
+      let randy = Math.floor(miny + Math.random() * (maxy - miny));
+      posy.push(randy);
+    }
+
+    //-------------------------
+    let wg = [];
+    const minWg = 0.0;
+    const maxWg = 10.0;
+    for (let index = 0; index < 50; index++) {
+      let ranWg = (minWg + Math.random() * (maxWg - minWg)).toFixed(2);
+      wg.push(ranWg);
+    }
+
+    //-------------------------
+    let tempPosition = [];
+    posx.map((posXData, idx) => {
+      tempPosition.push({
+        x: posXData,
+        y: posy[idx],
+        weight: wg[idx],
+      });
+    });
+    setPosi(tempPosition);
+  };
+
+  return (
+    <div>
+      <button
+        onClick={randomXYW}
+        style={{ marginLeft: 300, marginBottom: 50, marginTop: 100 }}
+      >
+        random xy
+      </button>
+      <div style={{ marginLeft: 300, marginBottom: 50 }}>
+        min : {filterValue[0]} <br/>
+        max : {filterValue[1]} 
       </div>
-    );
-}
+      {/* {console.log(filterValue[0])} */}
+      <Stage width={window.innerWidth} height={window.innerHeight}>
+        <Layer>
+          <RiceImage />
+          {posi.filter(item=>item.x>300||item.x<100).map((item, i) => {
+            // console.log(item);
+            return (
+              <Circle
+                x={item.x + 300}
+                y={item.y}
+                radius={25}
+                stroke="yellow"
+                key={i}
+              />
+            );
+          })}
+        </Layer>
+      </Stage>
+    </div>
+  );
+};
 
 export default Task1_2;
