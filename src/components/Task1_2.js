c7792ba5d43b0f569931650a011ddedb1413acb6
DomeNTPS <tanndome_555@hotmail.com>
2020-09-16 15:30:06 +0700

    get data from slider to filter weight

--- src/components/Task1_2.js
+++ src/components/Task1_2.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Stage, Layer, Circle, Image } from "react-konva";
 import useImage from "use-image";
 
-export const Task1_2 = ({ filterValue }) => {
+export const Task1_2 = ({ filterValue  }) => {
   let pos = { x: 0, y: 0, weightps: 0 };
   const [posi, setPosi] = React.useState([pos]);
 
@@ -60,26 +60,32 @@ export const Task1_2 = ({ filterValue }) => {
       >
         random xy
       </button>
+      {console.log(posi)}
       <div style={{ marginLeft: 300, marginBottom: 50 }}>
-        min : {filterValue[0]} <br/>
-        max : {filterValue[1]} 
+        min : {filterValue[0]} <br />
+        max : {filterValue[1]}
       </div>
       {/* {console.log(filterValue[0])} */}
       <Stage width={window.innerWidth} height={window.innerHeight}>
         <Layer>
           <RiceImage />
-          {posi.filter(item=>item.x>300||item.x<100).map((item, i) => {
-            // console.log(item);
-            return (
-              <Circle
-                x={item.x + 300}
-                y={item.y}
-                radius={25}
-                stroke="yellow"
-                key={i}
-              />
-            );
-          })}
+          {posi
+            .filter(
+              (item) =>
+                 item.weightps < 10 
+            )
+            .map((item, i) => {
+              // console.log(item);
+              return (
+                <Circle
+                  x={item.x + 300}
+                  y={item.y}
+                  radius={5}
+                  stroke="red"
+                  key={i}
+                />
+              );
+            })}
         </Layer>
       </Stage>
     </div>
